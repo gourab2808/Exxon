4eeadbbb0e13bb91810e41ae91e5e86493c32ff1
gourab2808 <gourab.sasmal96a@gmail.com>
2025-07-23 09:32:53 +0530

    upload file

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    subscription_id = "8047b6da-5aca-4a1b-a741-1c075ee56791"
+  features {}
+}
